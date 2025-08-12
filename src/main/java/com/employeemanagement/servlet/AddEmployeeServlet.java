3cd8e22cb849a2e806e8b73293d971f41112836e
venkatesh peta <85110131+techizone@users.noreply.github.com>
2025-08-12 20:55:39 +0530

    Update AddEmployeeServlet.java

--- src/main/java/com/employeemanagement/servlet/AddEmployeeServlet.java
+++ src/main/java/com/employeemanagement/servlet/AddEmployeeServlet.java
@@ -11,6 +11,14 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
 @WebServlet("/addEmployee")
 public class AddEmployeeServlet extends HttpServlet {
     private EmployeeDAO employeeDAO;
