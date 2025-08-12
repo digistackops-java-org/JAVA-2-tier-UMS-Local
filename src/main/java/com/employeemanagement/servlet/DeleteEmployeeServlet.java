e9d7d423c36fbdd71abdddf0cc6a753605ffba52
venkatesh peta <85110131+techizone@users.noreply.github.com>
2025-08-12 20:55:53 +0530

    Update DeleteEmployeeServlet.java

--- src/main/java/com/employeemanagement/servlet/DeleteEmployeeServlet.java
+++ src/main/java/com/employeemanagement/servlet/DeleteEmployeeServlet.java
@@ -10,6 +10,14 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
 @WebServlet("/deleteEmployee")
 public class DeleteEmployeeServlet extends HttpServlet {
     private EmployeeDAO employeeDAO;
