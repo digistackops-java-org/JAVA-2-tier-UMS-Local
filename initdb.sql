d684ab5c091a544ec22f30e43978944ff4f3d188
venkatesh peta <85110131+DigiStackOps@users.noreply.github.com>
2025-08-22 11:38:11 +0530

    Update initdb.sql

--- initdb.sql
+++ initdb.sql
@@ -1,32 +1,16 @@
--- Create database
-CREATE DATABASE IF NOT EXISTS employeedb;
-
--- Create user (if not exists)
-CREATE USER IF NOT EXISTS appuser WITH PASSWORD 'P@55Word';
-
--- Grant privileges on the DB
+-- Run this in the 'postgres' database
+CREATE DATABASE employeedb;
+CREATE USER appuser WITH PASSWORD 'P@55Word';
 GRANT ALL PRIVILEGES ON DATABASE employeedb TO appuser;
-
-\connect employeedb;
-
--- Create tables
-CREATE TABLE IF NOT EXISTS employee (
+\c employeedb;
+CREATE TABLE employee (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
     designation VARCHAR(100) NOT NULL,
     salary NUMERIC(10,2) NOT NULL
 );
-
 GRANT SELECT, INSERT, UPDATE, DELETE ON employee TO appuser;
 GRANT USAGE ON SCHEMA public TO appuser;
 GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO appuser;
-
-
--- Insert dummy employee data
-INSERT INTO employee (name, email, designation, salary) VALUES
-('Venkatesh', 'venkatesh@example.com', 'DevSecOps Engineer', 75000.00),
-('Chaitanya', 'chaitanya@example.com', 'Jr Devops Engineer', 68000.00),
-('Padol', 'padol@example.com', 'Sap Developer', 72000.00),
-('Ganesh', 'ganesh@example.com', 'Manager', 65000.00),
-('Pandu', 'pandu@example.com', 'System Analyst', 90000.00);
+SELECT * FROM employee;
